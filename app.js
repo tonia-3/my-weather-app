157b92c115d8d5ee114ef3e239adc2d44806b514
Tonia Chebet <93174528+tonia-3@users.noreply.github.com>
2023-07-22 22:51:31 +0300

    created the js file and added real life data using a weather API

---
+++ app.js
@@ -0,0 +1,15 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+let city = "Nairobi";
+let apiKey = "a7869b3482262ted8c4do104246adf4f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
