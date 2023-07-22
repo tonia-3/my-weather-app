5f75f89a31604f7f304962216cc3a016a4e006ad
Tonia Chebet <93174528+tonia-3@users.noreply.github.com>
2023-07-23 00:07:22 +0300

    added the correct icon from the API

--- app.js
+++ app.js
@@ -28,11 +28,18 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 let city = "Nairobi";
 let apiKey = "a7869b3482262ted8c4do104246adf4f";
